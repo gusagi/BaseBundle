a144f96db5cbed24bf6d99c72c37f01dd3ce14d6
Makoto Hashiguchi <gusagi@gmail.com>
2015-06-03 00:44:53 +0900

    Create base service class for Wizin package

---
+++ Service/Service.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Base service class for Wizin package
+ */
+namespace Wizin\Bundle\BaseBundle\Service;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Service extends ContainerAware
+{
+}
+
